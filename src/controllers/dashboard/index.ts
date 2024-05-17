94c2a66552d5c4b3b73b14fba3c6f6bd9162b886
zobaidul kazi <zobaidulkazi64.official@gmail.com>
2024-05-17 23:13:06 +0600

    get dashboard is auth

--- src/controllers/dashboard/index.ts
+++ src/controllers/dashboard/index.ts
@@ -1,2 +1,2 @@
-export { default as authDashboard } from "./authDashboard";
+
 export { default as dashboard } from "./dashboard";
--- src/routes/dashboard/getDashboard.ts
+++ src/routes/dashboard/getDashboard.ts
@@ -1,9 +1,7 @@
 import express from "express";
 const router = express.Router();
-import { authDashboard, dashboard } from "@/controllers/dashboard";
+import { dashboard } from "@/controllers/dashboard";
 import authMiddleware from "@/middleware/authMiddleware";
-
-router.post("/dashboard", authMiddleware, authDashboard);
 
 router.get("/dashboard", authMiddleware, dashboard);
 
