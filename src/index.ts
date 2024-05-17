503f3d137a16a032e5687cdf92e92549f346596e
zobaidul kazi <zobaidulkazi64.official@gmail.com>
2024-05-17 07:24:12 +0600

    live server build test

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
 import app from "./app";
-// import db from "./config/dbUrl";
+import db from "./config/dbUrl";
 import "dotenv/config";
 import { registration, login, getDashboard } from "./routes";
 import mongoose from "mongoose";
 
 import notification from "@/routes/notification";
-import { SenderController } from "./controllers/notification/sending";
-
-import { receiveNotifications } from "./controllers/notification/Notification";
 
 app;
-// db;
+db;
 
-mongoose
-  .connect("mongodb://localhost:27017/auth_mechanisms")
-  .then(() => {
-    console.log("Connected to MongoDB success");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// mongoose
+//   .connect("mongodb://localhost:27017/auth_mechanisms")
+//   .then(() => {
+//     console.log("Connected to MongoDB success");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // all the routes are defined here
 
@@ -30,8 +27,3 @@ app.use("/", dashboard);
 app.use("/auth", allRoutes);
 
 app.use("/", notification);
-
-async () => {
-  await SenderController.getInstance();
-  await receiveNotifications();
-};
--- src/routes/notification/index.ts
+++ src/routes/notification/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
-import { sendNotification } from "@/controllers/notification/Notification";
+// import { sendNotification } from "@/controllers/notification/Notification";
 
-router.post("/notification", sendNotification);
+// router.post("/notification", sendNotification);
 
 export default router;
