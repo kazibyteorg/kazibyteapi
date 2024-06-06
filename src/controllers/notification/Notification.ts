e230fa56f168e8a7d1334b0490343a4f80ddb634
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-06-06 22:52:57 +0600

    notifications send now not init

--- src/controllers/notification/Notification.ts
+++ src/controllers/notification/Notification.ts
@@ -1,29 +1,29 @@
-// import { Request, Response, NextFunction } from "express";
-// import redisClient from "@/utils/redisClient";
+import { Request, Response, NextFunction } from "express";
+import redisClient from "@/utils/redisClient";
 
-// const CHANNEL = "notifications";
+const CHANNEL = "notifications";
 
-// export const sendNotification = async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   const { message } = req.body;
-//   if (!message) {
-//     res.status(400).send("Message is required");
-//     return;
-//   }
-//   await redisClient.publish(CHANNEL, message);
-//   console.log(`Sent: ${message}`);
-//   res.send("Notification sent");
-// };
+export const sendNotification = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { message } = req.body;
+  if (!message) {
+    res.status(400).send("Message is required");
+    return;
+  }
+  await redisClient.publish(CHANNEL, message);
+  console.log(`Sent: ${message}`);
+  res.send("Notification sent");
+};
 
-// export const receiveNotifications = async () => {
-//   const subscriber = redisClient.duplicate();
-//   await subscriber.connect();
-//   await subscriber.subscribe(CHANNEL, (message) => {
-//     console.log(`Received: ${message}`);
-//     // Handle the received message, e.g., send it to users via WebSocket or any other method.
-//   });
-//   console.log("Subscribed to notifications channel");
-// };
+export const receiveNotifications = async () => {
+  const subscriber = redisClient.duplicate();
+  await subscriber.connect();
+  await subscriber.subscribe(CHANNEL, (message) => {
+    console.log(`Received: ${message}`);
+    // Handle the received message, e.g., send it to users via WebSocket or any other method.
+  });
+  console.log("Subscribed to notifications channel");
+};
