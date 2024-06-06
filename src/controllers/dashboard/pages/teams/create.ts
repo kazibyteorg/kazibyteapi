83f671ad6d122f42ebe4dde68adef8c565e181fb
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-06-06 23:14:00 +0600

    return sucees

--- src/controllers/dashboard/pages/teams/create.ts
+++ src/controllers/dashboard/pages/teams/create.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import FAQ from "@/models/dashboard/pages/teams/Teams";
+import Team from '@/models/dashboard/pages/teams/Teams'
 import { teamSchemas } from '@/schemas/dashboard/pages/Teams/teamSchemas'
 
 const createTeams = async (req: Request, res: Response, next: NextFunction) => {
@@ -15,26 +15,26 @@ const createTeams = async (req: Request, res: Response, next: NextFunction) => {
     }
 
     // Check if question & answer already exists
-    const existingFAQ = await FAQ.findOne({
-      
+    const existingTeam = await Team.findOne({
+      name: parsedBody.data.name,
     });
-    if (existingFAQ) {
+    if (existingTeam) {
       return res.status(400).json({
         success: false,
         message: "FAQ already exists",
       });
     }
 
-    // Create new FAQ
+    // Create new Team
 
-    const newFAQ = await FAQ.create({
+    const newFAQ = await Team.create({
       ...parsedBody.data,
     });
 
     // Return success response
     return res.status(200).json({
       success: true,
-      message: "FAQ created successfully",
+      message: "Team created successfully",
       newFAQ,
     });
   } catch (error) {
