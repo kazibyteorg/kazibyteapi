3cc21d721b70e7b4b3e72f65ecfa612541159479
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-06-06 23:18:00 +0600

    get all teams and pagination add controllers

--- src/controllers/dashboard/pages/teams/getAll.ts
+++ src/controllers/dashboard/pages/teams/getAll.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from "express";
-import FAQ from "@/models/dashboard/pages/FAQ/FAQ";
+import Team from "@/models/dashboard/pages/teams/Teams"
 
-// Controller to handle fetching all FAQs with pagination
-const getAllFAQs = async (req: Request, res: Response, next: NextFunction) => {
+// Controller to handle fetching all Teams with pagination
+const getAllTeams = async (req: Request, res: Response, next: NextFunction) => {
   try {
     // Extract query parameters for pagination
     const page = parseInt(req.query.page as string) || 1;
@@ -11,14 +11,14 @@ const getAllFAQs = async (req: Request, res: Response, next: NextFunction) => {
     // Calculate the starting index of the results for the current page
     const startIndex = (page - 1) * limit;
 
-    // Fetch the total count of FAQs for pagination calculation
-    const totalFAQs = await FAQ.countDocuments();
+    // Fetch the total count of Teams for pagination calculation
+    const totalTeams = await Team.countDocuments();
 
-    // Fetch the FAQs for the current page with the specified limit
-    const faqs = await FAQ.find().skip(startIndex).limit(limit);
+    // Fetch the Teams for the current page with the specified limit
+    const Teams = await Team.find().skip(startIndex).limit(limit);
 
     // Calculate total number of pages
-    const totalPages = Math.ceil(totalFAQs / limit);
+    const totalPages = Math.ceil(totalTeams / limit);
     const hasNextPage = page < totalPages;
     const hasPreviousPage = page > 1;
     const nextPage = hasNextPage ? page + 1 : null;
@@ -27,11 +27,11 @@ const getAllFAQs = async (req: Request, res: Response, next: NextFunction) => {
     // Return paginated response
     return res.status(200).json({
       success: true,
-      message: "FAQs retrieved successfully",
+      message: "Teams retrieved successfully",
       currentPage: page,
       totalPages,
-      totalFAQs,
-      faqs,
+      totalTeams,
+      Teams,
       hasNextPage,
       hasPreviousPage,
       nextPage,
@@ -43,4 +43,4 @@ const getAllFAQs = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export default getAllFAQs;
+export default getAllTeams;
